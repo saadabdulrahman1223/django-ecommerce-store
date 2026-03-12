867db93d554fd42b6e093d52bdb3f6a103207762
saadabdulrahman1223 <saadabdulrahman1223@gmail.com>
2026-03-12 12:48:46 +0530

    Add static media URL handling in debug mode

--- ecommerse_project/urls.py
+++ ecommerse_project/urls.py
@@ -24,5 +24,8 @@ urlpatterns = [
     path("products/", include("products.urls")),
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
